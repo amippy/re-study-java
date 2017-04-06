c59c6cbb586244ccef284b8e8d9ff19a603d935b
amippy <aminaka1223@gmail.com>
2017-04-06 16:05:04 +0900

    how to addition

---
+++ Sample3.java
@@ -0,0 +1,19 @@
+import java.io.*;
+
+class Sample3{
+  public static void main(String[] args)throws IOException{
+    System.out.println("put 2 integers");
+
+    BufferedReader br = 
+      new BufferedReader(new InputStreamReader(System.in));
+
+    String str1 = br.readLine();
+    String str2 = br.readLine();
+
+    int num1 = Integer.parseInt(str1);
+    int num2 = Integer.parseInt(str2);
+
+    System.out.println("result of addition is "+ (num1 + num2));
+
+  }
+}
