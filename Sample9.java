4cef080995d89c14f9bb495a79f8a6ee7af91c80
amippy <aminaka1223@gmail.com>
2017-04-07 15:31:37 +0900

    double to int

---
+++ Sample9.java
@@ -0,0 +1,12 @@
+class Sample9{
+  public static void main(String[] args){
+    double dnum = 170.2;
+
+    System.out.println("my height is "+ dnum + "cm");
+
+    System.out.println("change type ");
+
+    int inum = (int)dnum;
+    System.out.println("my height is"+ inum + "cm");
+  }
+}
