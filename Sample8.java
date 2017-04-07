2a8f5b4b3d9e3839c2b02d7c1bccd55719299cd5
amippy <aminaka1223@gmail.com>
2017-04-07 15:21:24 +0900

    int to double

---
+++ Sample8.java
@@ -0,0 +1,11 @@
+class Sample8{
+  public static void main(String[] args){
+
+    int inum = 170;
+
+    System.out.println("my height is"+ inum +"cm");
+
+    double dnum = inum;
+    System.out.println("my height is" + dnum + "cm");
+  }
+}
